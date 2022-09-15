55534eb072e2114a018d31c83af39b6f24562acf
Yuhan <102438276+zyaire11@users.noreply.github.com>
2022-09-14 22:10:59 -0400

    Create exercise1.5.cpp

---
+++ Ex2/src/exercise1.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+    int v1 = 2;
+    int v2 = 5;
+    
+    std::cout << "The sum of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 + v2;
+    std::cout << std::endl;
+
+    return 0;
+    
+}

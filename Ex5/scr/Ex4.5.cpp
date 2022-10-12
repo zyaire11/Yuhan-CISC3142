d0d1624c51ec84630b925bc4d2d4ae0590d788af
Yuhan <102438276+zyaire11@users.noreply.github.com>
2022-10-12 19:31:59 -0400

    Create Ex4.5.cpp

---
+++ Ex5/scr/Ex4.5.cpp
@@ -0,0 +1,20 @@
+/*
+Exercise 4.5: Determine the result of the following expressions.
+(a) -30 * 3 + 21 / 5
+(b) -30 + 3 * 21 / 5
+(c) 30 / 3 * 21 % 5
+(d) -30 / 3 * 21 % 4
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "the answer of -30 * 3 + 21 / 5 is " << -30 * 3 + 21 / 5 << "\n";
+    cout << "the answer of -30 + 3 * 21 / 5 is " << -30 + 3 * 21 / 5 << "\n";
+    cout << "the answer of 30 / 3 * 21 % 5 is " << 30 / 3 * 21 % 5 << "\n";
+    cout << "the answer of -30 / 3 * 21 % 4 is " << -30 / 3 * 21 % 4 << "\n";
+
+return 0;    
+}
